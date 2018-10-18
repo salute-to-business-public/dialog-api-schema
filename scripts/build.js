a0f74e5ca2b777e1f72bf62b0ea7f5c1f6f59c2c
Nikita Gusakov <gusnkt@gmail.com>
2018-10-18 11:27:46 +0300

    feat(js): add index js/ts

--- scripts/build.js
+++ scripts/build.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const fs = require('fs');
 const pbjs = require('protobufjs/cli/pbjs');
+const pbts = require('protobufjs/cli/pbts');
 const { promisify } = require('util');
 const _mkdirp = require('mkdirp');
 
 const mkdirp = promisify(_mkdirp);
+const copyFile = promisify(fs.copyFile);
 const writeFile = promisify(fs.writeFile);
 const compile = promisify(pbjs.main.bind(pbjs));
+const compileTs = promisify(pbts.main.bind(pbts));
 
 async function build({ include, protos }) {
   const output = path.join(__dirname, '../js');
@@ -25,6 +28,16 @@ async function build({ include, protos }) {
     ...protos
   ]);
   await writeFile(path.join(output, 'api.js'), js);
+
+  await compileTs([
+    '-o', path.join(output, 'index.d.ts'),
+    path.join(output, 'api.js')
+  ]);
+
+  await copyFile(
+    path.join(__dirname, '../src/js/index.js'), 
+    path.join(output, 'index.js')
+  );
 
   const proto = await compile(['-t', 'proto3', '-p', include, ...protos]);
   await writeFile(path.join(output, 'api.proto'), proto);
---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2018 Dialog LLC <info@dlg.im>
+ */
+
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+
+const packageDefinition = protoLoader.loadSync(
+  require.resolve('./api.proto'),
+  { keepCase: false }
+);
+
+const api = grpc.loadPackageDefinition(packageDefinition);
+
+module.exports = api;
