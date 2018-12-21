9d3fbcec7243ae46eb1a6a1c90d59a7aef31b21a
Andrey Skiba <a.skiba@dlg.im>
2018-12-21 17:41:23 +0300

    fix(python): remove init generation

--- scripts/py.sh
+++ scripts/py.sh
@@ -17,13 +17,4 @@ python3 -m grpc_tools.protoc \
 --grpc_python_out=/out \
 /project/include/scalapb/scalapb.proto
 
-touch /out/__init__.py
-echo "import six
-PATH_WORKAROUND = six.PY3
-
-if PATH_WORKAROUND:
-    import sys
-    import os
-    sys.path.append(os.path.dirname(__file__))
-" > /out/__init__.py
 touch /out/scalapb/__init__.py
